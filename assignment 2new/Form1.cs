c6dec6b7ab2b3f63f08ade2ade3c41b5b5857258
Jack Lowe <rc0030809@student.riversidecollege.ac.uk>
2019-01-15 09:34:58 +0000

    Working

--- assignment 2new/Form1.cs
+++ assignment 2new/Form1.cs
@@ -16,5 +16,99 @@ namespace assignment_2new
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Input, Input2, Input3, Input4, Input5;
+            try
+            {
+                Input = double.Parse(textBox1.Text);
+            }
+            catch
+            {
+                Input = 0;
+            }
+            try
+            {
+                Input2 = double.Parse(textBox2.Text);
+            }
+            catch
+            {
+                Input2 = 0;
+            }
+            try
+            {
+                Input3 = double.Parse(textBox3.Text);
+            }
+            catch
+            {
+                Input3 = 0;
+            }
+            try
+            {
+                Input4 = double.Parse(textBox4.Text);
+            }
+            catch
+            {
+                Input4 = 0;
+            }
+            try
+            {
+                Input5 = double.Parse(textBox5.Text);
+            }
+            catch
+            {
+                Input5 = 0;
+            }
+            double Answer = Input + Input2 + Input3 + Input4 + Input5
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double Input, Input2, Input3, Input4, Input5;
+            try
+            {
+                Input = double.Parse(textBox1.Text);
+            }
+            catch
+            {
+                Input = double.PositiveInfinity;
+            }
+            try
+            {
+                Input2 = double.Parse(textBox2.Text);
+            }
+            catch
+            {
+                Input2 = double.PositiveInfinity;
+            }
+            try
+            {
+                Input3 = double.Parse(textBox3.Text);
+            }
+            catch
+            {
+                Input3 = double.PositiveInfinity;
+            }
+            try
+            {
+                Input4 = double.Parse(textBox4.Text);
+            }
+            catch
+            {
+                Input4 = double.PositiveInfinity;
+            }
+            try
+            {
+                Input5 = double.Parse(textBox5.Text);
+            }
+            catch
+            {
+                Input5 = double.PositiveInfinity;
+            }
+            double Answer = 1.0 / (1.0 / Input + 1.0 / Input2 + 1.0 / Input3 + 1.0 / Input4 + 1.0 / Input5 + 1.0 /);
+            label1.Text = Answer.ToString();
+        }
     }
 }
---
+++ assignment 2new/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment_2new
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+             Input = double.Parse(textBox1.Text);
+            }
+            catch
+            {
+                Input = double.PositiveInfinity
+            }
+             double Answer = 1.0 / (1.0 / Input + 1.0 / Input2 + 1.0 / Input3);
+            label1.Text = Answer.ToString();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
